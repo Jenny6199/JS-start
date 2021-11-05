0eaff5f6110292d827360046a5ac172f0b8cd639
Jenny6199 <Jenny6199@yandex.ru>
2021-11-05 23:13:58 +0300

    complite_task_2_walker

---
+++ lesson_4/task_2/game.js
@@ -0,0 +1,31 @@
+let game = {
+    /**
+     * Запуск игры.
+     */
+    run() {
+        while(true) {
+            // Пользователь вводит направление следующего хода.
+            const direction = mover.getDirection();
+            if (direction === null) {
+                console.log("Игра завершена.")
+                return;
+            };
+
+            const nextPoint = mover.getNextPosition(direction);
+            renderer.clear();
+            player.move(nextPoint);
+            renderer.render();
+        };
+    },
+    
+    /**
+     * Инициализирует игру.
+     */
+    init() {
+        console.log("Положение игрока в консоли отмечено символом 'o'.")
+        renderer.render();
+        console.log("Для начала игры введите game.run() и нажмите Enter.")
+    },
+};
+
+game.init();
