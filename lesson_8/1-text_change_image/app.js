c675ba7240e62cd6c442c083c655f5bbdb527141
Jenny6199 <Jenny6199@yandex.ru>
2021-11-22 21:42:02 +0300

    complite_task_1

--- lesson_8/1-text_change_image/app.js
+++ lesson_8/1-text_change_image/app.js
@@ -1,36 +1,31 @@
 'use strict';
-// 1. получите все кнопки и сохраните в переменную
-
-// 1.1 затем проитерируйтесь по кнопкам и каждой из
-// них добавьте обработчик клика - функцию handleClick
-
+// Сохраняем в переменную ссылку на коллекцию кнопок.
+let buttons = document.querySelectorAll('button');
+// Каждой кнопке добавляем обработчик события по 'клику'
+buttons.forEach(product_button => {
+    product_button.addEventListener('click', handleClick, true);
+})
 
 /**
  * Функция обрабатывает клик по кнопке в карточке товара и попеременно вызывает
  * функции для показа или скрытия текста о товаре.
  * @param {MouseEvent} clickedButtonEvent 
  */
-function handleClick(clickedButtonEvent) {
-    // 2. из объекта события получите ссылку на .product и
-    // сохраните в переменную:
-    // const cardNode = ;
-
-    // 3. создайте литерал объекта со следующими свойствами:
+function handleClick(event) {
+    // Получаем ссылку на объект из события.
+    const cardNode = event.target.parentNode
+    // Cоздаем объект
     const card = {
-        wrap: , // здесь элемент с классом .product
-        img: , // здесь картинка внутри .product
-        productName: , // здесь .productName, который внутри .product
-        button: , // здесь button, который внутри .product
+        wrap: cardNode, 
+        img: cardNode.querySelector('img'),
+        productName: cardNode.querySelector('.productName'),
+        button: cardNode.querySelector('button'),
     };
-
-    // 4. получаем текст на кнопке, которая внутри .product
-    
-    
-    if () { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
-        // 4.2 если да, то передаем объект card в функцию showMoreText
+    // Получаем текст на кнопке
+    let button_text = card.button.textContent;
+    if (button_text == 'Подробнее') { 
         showMoreText(card);
-    } else if () { // 4.3 проверяем равняется ли текст на кнопке строке "Отмена"
-        // 4.4 если да, то передаем объект card в функцию hideMoreText
+    } else if (button_text == "Отмена") {
         hideMoreText(card);
     }
 }
@@ -38,35 +33,21 @@ function handleClick(clickedButtonEvent) {
 /**
  * Функция скрывает текст с описанием товара.
  * @param {Object} card 
- * @param {HTMLDivElement} card.wrap
- * @param {HTMLImageElement} card.img
- * @param {HTMLDivElement} card.productName
- * @param {HTMLButtonElement} card.button
  */
 function hideMoreText(card) {
-    // 5. картинке внутри .product ставим стиль display: block
-    
-    // 5.1 внутри .product находим элемент с классом .desc и удаляем его
-    
-    // 5.2 кнопке, которая внутри .product ставим текст "Подробнее"
-    
+    card.img.style.display = 'block'
+    let moreText = card.wrap.querySelector('.desc');
+    card.wrap.removeChild(moreText);
+    card.button.textContent = 'Подробнее';
 }
 
 /**
  * Функция показывает текст с описанием товара.
  * @param {Object} card 
- * @param {HTMLDivElement} card.wrap
- * @param {HTMLImageElement} card.img
- * @param {HTMLDivElement} card.productName
- * @param {HTMLButtonElement} card.button 
  */
 function showMoreText(card) {
-    // 6. картинке внутри .product ставим display: none
-    
-    // 6.1 сохраняем произвольный текст в переменную
-    
-    // 6.2 внутри .product есть .productName, после него вставляем div.desc и текстом из переменной из п. 6.1
-    
-    // 6.3 внутри .product у кнопки текст ставим "Отмена"
-    
+    card.img.style.display = 'none';
+    let moreText = 'Более подробное описание товара, пожалуйста ознакомтесь';
+    card.productName.insertAdjacentHTML('afterend', `<div class="desc">${moreText}</div>`);
+    card.button.textContent = 'Отмена';
 }
